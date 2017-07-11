a58623b990cb8c1de72cc1ba1aa30b2b1eb9526f
Shravali <shravali@gmail.com>
2017-07-11 18:16:42 -0400

    first commit

--- test/app.js
+++ test/app.js
@@ -61,3 +61,11 @@ function deleteMessage(req, res) {
             }
         );
 }
+
+var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds149382.mlab.com:49382/heroku_86pj5q6n'; // user yours
+}
