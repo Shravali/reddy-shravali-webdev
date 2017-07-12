ffa8c926364c931eab5fbdd68b4f2ec035f08c7c
Shravali <shravali@gmail.com>
2017-07-12 16:59:39 -0400

    fixing connection strung

--- test/app.js
+++ test/app.js
@@ -4,15 +4,24 @@ app.get("/api/test", findAllMessages);
 app.post("/api/test", createMessage);
 app.delete("/api/test/:id", deleteMessage);
 
-var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-if(process.env.MLAB_USERNAME) {
-    connectionString = process.env.MLAB_USERNAME + ":" +
-        process.env.MLAB_PASSWORD + "@" +
-        process.env.MLAB_HOST + ':' +
-        process.env.MLAB_PORT + '/' +
-        process.env.MLAB_APP_NAME;
+var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds149382.mlab.com:49382/heroku_86pj5q6n'; // user yours
+
 }
+
+//
+// if(process.env.MLAB_USERNAME) {
+//     connectionString = process.env.MLAB_USERNAME + ":" +
+//         process.env.MLAB_PASSWORD + "@" +
+//         process.env.MLAB_HOST + ':' +
+//         process.env.MLAB_PORT + '/' +
+//         process.env.MLAB_APP_NAME;
+// }
 
 var mongoose = require("mongoose");
 mongoose.connect(connectionString);
@@ -62,12 +71,4 @@ function deleteMessage(req, res) {
         );
 }
 
-var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    var password = process.env.MLAB_PASSWORD_WEBDEV;
-    connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds149382.mlab.com:49382/heroku_86pj5q6n'; // user yours
-
-}
 
